e621aa4aca9464a328d6487647390aa1226a7153
urkkiz225 <urkkiz225@gmail.com>
2026-03-07 04:15:59 +0200

    added centered video player, removed default content and fixed deployment bugs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactPlayer from 'react-player';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <ReactPlayer 
+        //runaa npm run build ja uudelleenkäynnistä servu npm startil jos pistät staticMediaan mitään uutta ni ei mee sekasi mikää
+        src = {process.env.PUBLIC_URL + "/staticMedia/rickastley.mp4"}
+        controls={false} 
+        width="90%" 
+        height="90%"
+        playing 
+        muted
+        config={{
+      }}
+      />
     </div>
   );
 }
