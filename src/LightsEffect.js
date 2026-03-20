4be4eb7504ec7721e9610cd0323891de7926fa01
urkkiz225 <urkkiz225@gmail.com>
2026-03-20 20:39:11 +0200

    bug fixes

--- src/LightsEffect.js
+++ src/LightsEffect.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, {useState, useMemo, useRef } from 'react';
 //shout out framer-motion pkg greatest animation package of all time
 import { motion, useScroll, useTransform, useSpring} from 'framer-motion';
 //perkele ku mistää ei löydy commercial use stock kuvii spotlight beameist nii tääki piti tehä ite perkele vittu! ᓚᘏᗢ
@@ -6,26 +6,30 @@ import spotLightBeam from './Assets/spotlight_texturedbeam_midres.png';
 import clubXLogo from './Assets/header.png';
 
 const LightsEffect = ({isMobile, computerIsNarrowScreenXOR}) => {
-  var portrait = !( window.screen.orientation.type === 'landscape-primary' 
-  || window.screen.orientation.type === 'landscape-secondary');
+  const [portrait, setPortrait] = useState(!(window.screen.orientation.type === 'landscape-primary' 
+  || window.screen.orientation.type === 'landscape-secondary'));
   window.screen.orientation.addEventListener('change', () => {
-      portrait = !(window.screen.orientation.type === 'landscape-primary' 
-    || window.screen.orientation.type === 'landscape-secondary');
+      setPortrait(!(window.screen.orientation.type === 'landscape-primary' 
+  || window.screen.orientation.type === 'landscape-secondary'));
   });
   //i STILL HATE userefs!!!1!!!!
   const containerRef = useRef(null);
+  const offset = (() => {
+    if (isMobile && !computerIsNarrowScreenXOR)
+      return portrait 
+        ? ["125vw start", "240vw start"] 
+        : ["10vw start", "120vw start"];
+    if (!computerIsNarrowScreenXOR) 
+      return ["20vw start", "145vw start"];
+    return ["30vw start", "155vw start"];
+    
+  })();
   const { scrollYProgress } = useScroll({
-    target:containerRef.current, offset: (isMobile && !computerIsNarrowScreenXOR)
-      ?(portrait?["125vw start","240vw start"]
-      :["10vw start","120vw start"])
-      :!computerIsNarrowScreenXOR 
-      ?["20vw start","145vw start"]
-      :["30vw start","155vw start"]
-      /*todo tee täst selkeempi (pikselivaluet?)*/
+    target:containerRef.current, offset: offset
   });
   const smoothProgress = useSpring(scrollYProgress, {stiffness: 250, damping: 40,
   restDelta: 0.001
-});
+  });
   const leftLightBeamRot = useTransform(smoothProgress, [0, 0.1, 0.5, 1], [90, 100, 160, 160]);
   const rightLightBeamRot = useTransform(smoothProgress, [0, 0.1, 0.5, 1], [90, 80, 20, 20]);
   const beamOpacity = useTransform(scrollYProgress, [0,0.3], [0,0.5])
